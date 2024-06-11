cd2968a156b99725f592991f2ebaeed80863ab31
Shaan Patel <shaanpatel1213@gmail.com>
2024-06-11 19:10:06 -0400

    Revising frontend tests

--- huskysheetsui/jest.config.js
+++ huskysheetsui/jest.config.js
@@ -1,10 +1,6 @@
 module.exports = {
     preset: 'ts-jest',
     testEnvironment: 'node',
-    transform: {
-        '^.+\\.tsx?$': 'ts-jest',
-        '^.+\\.jsx?$': 'babel-jest'
-    },
     moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
     transformIgnorePatterns: ['node_modules/(?!axios)'],
 };
