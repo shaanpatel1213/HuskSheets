fd0979cb73859d43c6f0bd366af04f2fe2f3a5e1
Emily Fink <emilyfink@Emilys-MacBook-Air.local>
2024-06-14 14:56:20 -0400

    Updated tests for utilities.test so it's now over 80% coverage

--- huskysheetsui/src/__tests__/utilities.test.ts
+++ huskysheetsui/src/__tests__/utilities.test.ts
@@ -17,29 +17,58 @@ const data: TableData = [
 describe('resolveOperand function', () => {
     test('correctly returns number when resolveOperand is given a number', () => {
         expect(resolveOperand(2, data)).toBe(2);
-    })
+    });
 
     test('correctly returns value of cell when resolveOperand is given a cell', () => {
         expect(resolveOperand('$A1', data)).toBe(1);
-    })
+    });
 
     test('correctly returns value of cell when resolveOperand is given a formula', () => {
         expect(resolveOperand('=2*3', data)).toBe('6');
-    })
+    });
+
+    test('correctly returns value of cell when resolveOperand is given a reference', () => {
+        expect(resolveOperand({ row: 1, col: 2 }, data)).toBe('6');
+    });
 });
 
 describe('resolveCellReference function', () => {
     test('correctly returns value of a given cell', () => {
         expect(resolveCellReference('$A2', data)).toBe(4);
-    })
+        expect(resolveCellReference('A2', data)).toBe(4);
+    });
+
+    test('correctly returns given string when it is not a cell reference', () => {
+        expect(resolveCellReference('hi', data)).toBe('hi');
+    });
+
+    test('correctly returns ERROR when given impossible cell reference', () => {
+        expect(resolveCellReference('$A0', data)).toBe('ERROR');
+    });
 });
 
 describe('parseCellReference function', () => {
-    
+    test('correctly returns correct row and column give a cell reference', () => {
+        expect(parseCellReference('$A1')).toStrictEqual({ row: 0, col: 0 });
+    });
+
+    test('correctly throws error when given an invalid cell reference', () => {
+        expect(() => {
+            parseCellReference('$1A');
+        }).toThrow('Invalid cell reference');
+    });
 });
 
 describe('getRangeFromReference function', () => {
-    
+    test('correctly returns the values of a given range of cells', () => {
+        expect(getRangeFromReference('$A1:$A3', data)).toStrictEqual(['1', '4', '7']);
+    });
+
+    test('correctly throws error when given an invalid range format', () => {
+        expect(() => {
+            getRangeFromReference('$A1-$A3', data);
+        }).toThrow('Invalid range format');
+    });
 });
 
 describe('colToIndex function', () => {
@@ -50,6 +79,6 @@ describe('colToIndex function', () => {
 
 describe('isRef function', () => {
     test('correctly checks if given parameter is a Ref', () => {
-        expect(isRef({row: 1, col: 2})).toBe(true);
+        expect(isRef({ row: 1, col: 2 })).toBe(true);
     });
 });
